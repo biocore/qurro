058da5bd035736bfcb3b2fe00c20ed4d3d2310a2
Marcus Fedarko <mfedarko@ucsd.edu>
2018-11-19 02:12:46 -0800

    Save rank plot stuff; update ranks on multi select
    Really good progress. next up is making this work for single-select

--- linked_plots.js
+++ linked_plots.js
@@ -10,6 +10,16 @@
 // or whatever. Uh I'm gonna just ignore this for now.
 
 var ssmv = {};
+ssmv.rankPlotView = undefined;
+ssmv.samplePlotView = undefined;
+// NOTE that these JSONs are not changed when their respective plots are.
+// However, this is ok: the main reason we keep these around is for the data
+// contained within them, which shouldn't change. We only change things that
+// we could recompute using the JSON anyway (i.e. the "balance" in the sample
+// plot, and the "classification" in the rank plot)
+ssmv.rankPlotJSON = {};
+ssmv.samplePlotJSON = {};
+// Used for selections of log ratios between single taxa (via the rank plot)
 ssmv.newTaxonLow = undefined;
 ssmv.newTaxonHigh = undefined;
 ssmv.oldTaxonLow = undefined;
@@ -17,13 +27,13 @@ ssmv.oldTaxonHigh = undefined;
 // For selections of potentially many taxa (not via the rank plot)
 ssmv.topTaxa = undefined;
 ssmv.botTaxa = undefined;
-ssmv.samplePlotJSON = {};
 // We set ssmv.selectMicrobes to undefined when no select microbes file has
 // been provided yet.
 ssmv.selectMicrobes = undefined;
 
 ssmv.makeRankPlot = function(spec) {
     vegaEmbed("#rankPlot", spec, {"actions": false}).then(function(result) {
+        ssmv.rankPlotView = result.view;
         // NOTE ideally we'd update this on dragover (since that'd
         // let us continuously update the scatterplot as we brush
         // the rank plot), but for some reason I can't get that to
@@ -33,7 +43,7 @@ ssmv.makeRankPlot = function(spec) {
         //
         // The paradigm of this temporary mechanism is: click down to
         // set the low taxon, release to set high taxon
-        result.view.addEventListener("mousedown", function(e, i) {
+        ssmv.rankPlotView.addEventListener("mousedown", function(e, i) {
             if (i !== null && i !== undefined) {
                 if (i["mark"]["marktype"] === "rect") {
                     ssmv.oldTaxonLow = ssmv.newTaxonLow;
@@ -44,7 +54,7 @@ ssmv.makeRankPlot = function(spec) {
                 }
             }
         });
-        result.view.addEventListener("mouseup", function(e, i) {
+        ssmv.rankPlotView.addEventListener("mouseup", function(e, i) {
             if (i !== null && i !== undefined) {
                 if (i["mark"]["marktype"] === "rect") {
                     ssmv.oldTaxonHigh = ssmv.newTaxonHigh;
@@ -182,7 +192,38 @@ ssmv.updateBalanceMulti = function(sampleRow) {
     return newBalance;
 };
 
-ssmv.changeSamplePlot = function(updateBalanceFunction) {
+// Given a "row" of data about a rank, return its new classification depending
+// on the new selection that just got made.
+ssmv.updateRankColorSingle = function(rankRow) {
+    return "Both";
+}
+
+ssmv.updateRankColorMulti = function(rankRow) {
+    var inTop = false;
+    var inBot = false;
+    if (ssmv.topTaxa.indexOf(rankRow["index"]) >= 0) {
+        inTop = true;
+    }
+    if (ssmv.botTaxa.indexOf(rankRow["index"]) >= 0) {
+        inBot = true;
+    }
+    if (inTop) {
+        if (inBot) {
+            return "Both";
+        }
+        else {
+            return "Numerator";
+        }
+    }
+    else if (inBot) {
+        return "Denominator";
+    }
+    else {
+        return "None";
+    }
+}
+
+ssmv.changeSamplePlot = function(updateBalanceFunc, updateRankColorFunc) {
     // Either modify the scatterplot with the new
     // balances, or make a new JSON data object for the
     // scatterplot with the new balances and just make the
@@ -207,7 +248,18 @@ ssmv.changeSamplePlot = function(updateBalanceFunction) {
         // sample we want to change)
         ssmv.samplePlotJSON["datasets"]["col_names"]["balance"],
         // function to run to determine what the new balances are
-        updateBalanceFunction
+        updateBalanceFunc
+    )).run();
+    // Update rank plot based on the new log ratio
+    // Storing this within changeSamplePlot() is a (weak) safeguard that
+    // changes to the state of the sample plot (at least done using the UI
+    // controls on the page, not the dev console) also propagate to the rank
+    // plot.
+    var rankDataName = ssmv.rankPlotJSON["data"]["name"];
+    ssmv.rankPlotView.change(rankDataName, vega.changeset().modify(
+        vega.truthy,
+        "classification",
+        updateRankColorFunc
     )).run();
 };
 
@@ -240,7 +292,7 @@ ssmv.updateSamplePlotMulti = function() {
         ssmv.botTaxa = ssmv.filterTaxaByPhrase($("#botText").val(), false,
             true);
     }
-    ssmv.changeSamplePlot(ssmv.updateBalanceMulti);
+    ssmv.changeSamplePlot(ssmv.updateBalanceMulti, ssmv.updateRankColorMulti);
     // Clear the single-microbe association text to be clearer
     var logRatioDisp = MathJax.Hub.getAllJax("logRatioDisplay")[0];
     MathJax.Hub.Queue(["Text", logRatioDisp, ""]);
@@ -273,7 +325,10 @@ ssmv.updateSamplePlotSingle = function() {
                     + ssmv.newTaxonLow.replace(/;/g, downLvl) + "}}\\bigg)";
                 MathJax.Hub.Queue(["Text", logRatioDisp, newEq]);
 
-                ssmv.changeSamplePlot(ssmv.updateBalanceSingle);
+                ssmv.changeSamplePlot(
+                    ssmv.updateBalanceSingle,
+                    ssmv.updateRankColorSingle
+                );
             }
         }
     }
@@ -321,17 +376,28 @@ ssmv.uploadSelectMicrobesFile = function() {
     }
 };
 
-ssmv.makeRankPlot("rank_plot.json");
-// Use an XMLHTTPRequest to get JSON for the sample plot, since we want to hang
-// on to the JSON for this file
-// Reference: http://www.henryalgus.com/reading-binary-files-using-jquery-ajax/
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "sample_logratio_plot.json");
-xhr.responseType = "json";
-xhr.onload = function(e) {
-    if (this.status === 200) {
-        ssmv.samplePlotJSON = this.response;
-        ssmv.makeSamplePlot(ssmv.samplePlotJSON);
-    }
-};
-xhr.send();
+// Run on page startup: load and save JSON files, and make plots accordingly
+var jsonsToLoad = ["rank_plot.json", "sample_logratio_plot.json"];
+for (var ji = 0; ji < 2; ji++) {
+    // Use an XMLHTTPRequest to get JSON for both plots, since we want to hang
+    // on to that instead of just passing it to vegaEmbed
+    // See http://www.henryalgus.com/reading-binary-files-using-jquery-ajax/
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", jsonsToLoad[ji]);
+    xhr.responseType = "json";
+    xhr.onload = function(e) {
+        if (this.status === 200) {
+            // By the time this function is called, ji is (probably) already 2.
+            // So we can't rely on it to figure out the JSON this XHR is for.
+            if (this.responseURL.endsWith("rank_plot.json")) {
+                ssmv.rankPlotJSON = this.response;
+                ssmv.makeRankPlot(this.response);
+            }
+            else {
+                ssmv.samplePlotJSON = this.response;
+                ssmv.makeSamplePlot(this.response);
+            }
+        }
+    };
+    xhr.send();
+}
