b48e6c84a9bb696787e69799b6268626432890be
Marcus Fedarko <mfedarko@ucsd.edu>
2018-12-17 14:55:42 -0800

    Add preliminary sample plot JSON validation

--- rankratioviz/tests/test_byrd_basic.py
+++ rankratioviz/tests/test_byrd_basic.py
@@ -8,14 +8,16 @@ def test_byrd():
     test_input_dir = os.path.join("data", "byrd")
     test_output_dir = os.path.join("rankratioviz", "tests", "output", "byrd")
 
-    input_rank_loc = os.path.join(test_input_dir, "beta.csv")
     rank_json_loc = os.path.join(test_output_dir, "rank_plot.json")
-    # Super basic initial "test." This just makes sure that we don't get any
-    # errors. (I'll make this actually test stuff soon.)
-    gen_plots.run_script(["-r", input_rank_loc,
-        "-t", os.path.join(test_input_dir, "byrd_skin_table.biom"),
-        "-m", os.path.join(test_input_dir, "byrd_metadata.txt"),
-        "-d", test_output_dir])
+    sample_json_loc = os.path.join(test_output_dir, "sample_logratio_plot.json")
+
+    input_rank_loc = os.path.join(test_input_dir, "beta.csv")
+    biom_table_loc = os.path.join(test_input_dir, "byrd_skin_table.biom")
+    metadata_loc = os.path.join(test_input_dir, "byrd_metadata.txt")
+
+    # Actually run the plot generation script
+    gen_plots.run_script(["-r", input_rank_loc, "-t", biom_table_loc,
+        "-m", metadata_loc, "-d", test_output_dir])
 
     # We use the ranks contained in column 3 (0-indexed) because they
     # correspond to the log(PostFlare/Flare) + K ranks: the column name for
@@ -23,4 +25,5 @@ def test_byrd():
     # When adapting this test to other datasets/rank files, you'll need to
     # adjust this parameter accordingly.
     testing_utilities.validate_rank_plot_json(input_rank_loc, rank_json_loc, 3)
-    # TODO validate sample scatterplot JSON
+    testing_utilities.validate_sample_plot_json(biom_table_loc, metadata_loc,
+            sample_json_loc)
--- rankratioviz/tests/testing_utilities.py
+++ rankratioviz/tests/testing_utilities.py
@@ -1,6 +1,11 @@
 import json
 from pytest import approx
 
+def basic_vegalite_json_validation(json_obj):
+    """Basic validations of Vega-Lite JSON objects go here."""
+    assert json_obj["$schema"].startswith("https://vega.github.io/schema")
+    assert json_obj["$schema"].endswith(".json")
+
 def validate_rank_plot_json(input_rank_loc, rank_json_loc, rank_col_index):
     # Record all taxa and the requested rank value in the input ranks file
     ranked_taxa = {}
@@ -23,8 +28,7 @@ def validate_rank_plot_json(input_rank_loc, rank_json_loc, rank_col_index):
         # matching our specs is being created)
         assert rank_plot["mark"] == "bar"
         assert rank_plot["title"] == "Ranks"
-        assert rank_plot["$schema"].startswith("https://vega.github.io/schema")
-        assert rank_plot["$schema"].endswith(".json")
+        basic_vegalite_json_validation(rank_plot)
         dn = rank_plot["data"]["name"]
         # Check that we have the same count of ranks as in the input ranks file
         assert len(rank_plot["datasets"][dn]) == len(ranked_taxa)
@@ -50,3 +54,14 @@ def validate_rank_plot_json(input_rank_loc, rank_json_loc, rank_col_index):
             # Update prev_ things for the next iteration of the loop
             prev_coefs_val = rank["coefs"]
             prev_x_val = rank["x"]
+
+def validate_sample_plot_json(biom_table_loc, metadata_loc, sample_json_loc):
+    with open(sample_json_loc, "r") as sampleplotfile:
+        sample_plot = json.load(sampleplotfile)
+        assert sample_plot["mark"] == "circle"
+        assert sample_plot["title"] == "Log Ratio of Abundances in Samples"
+        basic_vegalite_json_validation(sample_plot)
+        dn = sample_plot["data"]["name"]
+        # TODO check that all metadata samples are accounted for in BIOM table
+        # TODO check that every log ratio is correct! I guess that'll make us
+        # load the rank plots file, but it's worth it (tm)
