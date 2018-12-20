f237676828487232146aaa8ce595ea9a05cb2bf9
Marcus Fedarko <mfedarko@ucsd.edu>
2018-12-20 01:10:39 -0800

    --in_catagory -> --in_category

--- rankratioviz/scripts/_rank_plot.py
+++ rankratioviz/scripts/_rank_plot.py
@@ -11,16 +11,16 @@ import os
 from rankratioviz.generate import process_input,gen_rank_plot,gen_sample_plot
 
 @click.command()
-@click.option('--ranks', help='(str) Path to ordiantion \
-                                         output from deicode & minstel')
+@click.option('--ranks', help="""(str) Path to ordination output from a tool
+    like DEICODE, songbird, etc.""")
 @click.option('--in_biom', help='BIOM table describing taxon abundances for samples.')
 @click.option('--in_taxonomy', default=None, help='Metadata table file for taxonomy.')
 @click.option('--in_metadata', help='Metadata table file for samples.')
-@click.option('--in_catagory', help='Metadata table catagory to plot.')
+@click.option('--in_category', help='Metadata table category to plot.')
 @click.option('--output_dir', help='Location of output files.')
 
 def rank_plots(ranks: str, in_biom: str, in_metadata: str, 
-               output_dir: str, in_taxonomy: str , in_catagory: str) -> None:
+               output_dir: str, in_taxonomy: str , in_category: str) -> None:
 
     # import 
     in_biom = load_table(in_biom)
@@ -34,20 +34,20 @@ def rank_plots(ranks: str, in_biom: str, in_metadata: str,
 
     U, V, table, metadata = process_input(ranks, in_biom, in_metadata, in_taxonomy)
     rank_plot_chart = gen_rank_plot(U, V, 0)
-    sample_plot_json = gen_sample_plot(table, metadata, in_catagory)
+    sample_plot_json = gen_sample_plot(table, metadata, in_category)
     os.makedirs(output_dir, exist_ok=True)
     #write 
-    os.mkdir(os.path.join(output_dir,'rank_plot_'+in_catagory))
+    os.mkdir(os.path.join(output_dir,'rank_plot_'+in_category))
     # copy files for viz
     loc_ = os.path.dirname(os.path.realpath(__file__))
     for file_ in os.listdir(os.path.join(loc_,'data')):
         if file_ != '.DS_Store':
             copyfile(get_data_path(file_), 
-                     os.path.join(output_dir,'rank_plot_'+in_catagory,file_))
-    #write new filez
-    rank_plot_loc = os.path.join(output_dir,'rank_plot_'+in_catagory, 
+                     os.path.join(output_dir,'rank_plot_'+in_category,file_))
+    #write new files
+    rank_plot_loc = os.path.join(output_dir,'rank_plot_'+in_category,
                                  "rank_plot.json")
-    sample_plot_loc = os.path.join(output_dir,'rank_plot_'+in_catagory,
+    sample_plot_loc = os.path.join(output_dir,'rank_plot_'+in_category,
                                    "sample_logratio_plot.json")
 
     rank_plot_chart.save(rank_plot_loc)
