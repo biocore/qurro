047b4568dc193be8bf522dbf2c08a620399440b0
Marcus Fedarko <mfedarko@ucsd.edu>
2019-01-11 14:13:40 -0800

    minor __init__.py style tweaks

--- rankratioviz/__init__.py
+++ rankratioviz/__init__.py
@@ -1,11 +1,9 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2016--, rankratiotoviz development team.
+# Copyright (c) 2018--, rankratiotoviz development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file COPYING.txt, distributed with this software.
-#
-#
 # ----------------------------------------------------------------------------
 
 __version__ = "0.0.0"
