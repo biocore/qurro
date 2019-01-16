1e73c21ce14fff0b986a4ab43825d7ac0179e06a
Marcus Fedarko <mfedarko@ucsd.edu>
2019-01-15 16:02:59 -0800

    Refer to license file with correct name + add docs
    (The docs were added to the top comment in rankratioviz/__init__.py.
    This should help users who do "import rankratioviz" and then
    "help(rankratioviz)" in the Python prompt.)

--- rankratioviz/__init__.py
+++ rankratioviz/__init__.py
@@ -1,9 +1,13 @@
 # ----------------------------------------------------------------------------
+# rankratioviz is a visualization tool that displays ranked taxa (or
+# metabolites) alongside log ratios of their abundances. It can generate
+# visualizations either as a standalone tool or as a plugin for QIIME 2.
+#
 # Copyright (c) 2018--, rankratiotoviz development team.
 #
 # Distributed under the terms of the Modified BSD License.
 #
-# The full license is in the file COPYING.txt, distributed with this software.
+# The full license is in the file LICENSE.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 
 __version__ = "0.0.0"
