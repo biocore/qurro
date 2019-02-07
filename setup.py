4aef5d4057ed7fcd1cbd3cd49df5182ee26a0220
Marcus Fedarko <mfedarko@ucsd.edu>
2019-02-07 15:57:48 -0800

    adding back old setup code in attempt to fix pbm
    Yeah uh it looks like "pip install -e ." doesn't actually install
    packages akin to how installing to github goes, so sorry for all
    the extraneous commits to anyone who's copied on these messages

--- setup.py
+++ setup.py
@@ -11,8 +11,32 @@
 # ----------------------------------------------------------------------------
 
 import re
+import os
 import ast
 from setuptools import find_packages, setup
+from setuptools.command.build_ext import build_ext as _build_ext
+
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup
+        # process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
+
+
+# Dealing with Cython
+USE_CYTHON = os.environ.get('USE_CYTHON', False)
+ext = '.pyx' if USE_CYTHON else '.c'
+
+extensions = [
+]
+
+if USE_CYTHON:
+    from Cython.Build import cythonize
+    extensions = cythonize(extensions)
 
 classes = """
     Development Status :: 3 - Alpha
@@ -59,6 +83,8 @@ setup(
     maintainer="rankratioviz development team",
     maintainer_email="mfedarko@ucsd.edu",
     packages=find_packages(),
+    ext_modules=extensions,
+    cmdclass={'build_ext': build_ext},
     # TODO determine what versions we really care about. Also the ipython
     # versions differ btwn. here and ci/conda_requirements.txt; figure out
     # which is needed.
