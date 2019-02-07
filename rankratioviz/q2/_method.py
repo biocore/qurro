9ae01b761e3ceda0934f90f451fd3b3ac59dc820
Marcus Fedarko <mfedarko@ucsd.edu>
2019-02-07 11:09:20 -0800

    flake8 fixes in q2/_method.py
    This is the last of these, aside from (as mentioned) that one
    list comprehension in generate.py.

--- rankratioviz/q2/_method.py
+++ rankratioviz/q2/_method.py
@@ -6,7 +6,7 @@
 # The full license is in the file LICENSE.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 import os
-import json 
+import json
 import qiime2
 import skbio
 import biom
@@ -14,36 +14,35 @@ import q2templates
 from shutil import copyfile
 from rankratioviz.generate import process_input, gen_rank_plot, gen_sample_plot
 
-def plot(output_dir: str, table: biom.Table,
-                ranks: skbio.OrdinationResults, 
-                sample_metadata: qiime2.Metadata, 
-                feature_metadata: qiime2.Metadata , 
-                in_catagory: str) -> None:
+
+def plot(output_dir: str, table: biom.Table, ranks: skbio.OrdinationResults,
+         sample_metadata: qiime2.Metadata, feature_metadata: qiime2.Metadata,
+         in_catagory: str) -> None:
 
     # get data
-    U, V, table, metadata = process_input(ranks, table, 
-                                          sample_metadata.to_dataframe(), 
+    U, V, table, metadata = process_input(ranks, table,
+                                          sample_metadata.to_dataframe(),
                                           feature_metadata.to_dataframe())
     rank_plot_chart = gen_rank_plot(U, V, 0)
     sample_plot_json = gen_sample_plot(table, metadata, in_catagory)
-    # make dir 
-    os.makedirs(os.path.join(output_dir,'rank_plot'), exist_ok=True)
+    # make dir
+    os.makedirs(os.path.join(output_dir, 'rank_plot'), exist_ok=True)
     # copy files for viz
     # TODO: this is slightly unsafe -- if no .html file is found in data/, then
     # this will fail. Solution is to define index = None first, and then after
     # the loop double-check that index isn't None anymore. If it still is then
     # we can fail gracefully with an explanation.
     loc_ = os.path.dirname(os.path.realpath(__file__))
-    for file_ in os.listdir(os.path.join(loc_,'data')):
+    for file_ in os.listdir(os.path.join(loc_, 'data')):
         if file_ != '.DS_Store':
             copyfile(skbio.util.get_data_path(file_),
-                     os.path.join(output_dir,'rank_plot',file_))
+                     os.path.join(output_dir, 'rank_plot', file_))
         if '.html' in file_:
-            index = os.path.join(output_dir,'rank_plot',file_)
+            index = os.path.join(output_dir, 'rank_plot', file_)
     # write new files
-    rank_plot_loc = os.path.join(output_dir,'rank_plot', 
+    rank_plot_loc = os.path.join(output_dir, 'rank_plot',
                                  "rank_plot.json")
-    sample_plot_loc = os.path.join(output_dir,'rank_plot',
+    sample_plot_loc = os.path.join(output_dir, 'rank_plot',
                                    "sample_logratio_plot.json")
     rank_plot_chart.save(rank_plot_loc)
     with open(sample_plot_loc, "w") as jfile:
