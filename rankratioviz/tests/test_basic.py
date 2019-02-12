9fa566a1b7536c6e2827a59525748fe8de22f57d
Marcus Fedarko <mfedarko@ucsd.edu>
2019-02-11 18:04:07 -0800

    Simplify Travis install; add a very basic test
    This is a starting point from which we can move over the JSON
    validation code to the new input system (with ordination files
    instead of beta.csv files).

---
+++ rankratioviz/tests/test_basic.py
@@ -0,0 +1,23 @@
+import os
+from click.testing import CliRunner
+from rankratioviz import generate
+from rankratioviz.tests import testing_utilities
+import rankratioviz.scripts._plot as rrvp
+
+
+def test_basic():
+    eg_dir = "example/deicode_example/"
+    # Derived from http://click.palletsprojects.com/en/7.x/testing/
+    runner = CliRunner()
+    result = runner.invoke(rrvp.plot, [
+        "--ranks", eg_dir + "ordination.txt", "--abundance-table",
+        eg_dir + "qiita_10422_table.biom", "--sample-metadata",
+        eg_dir + "qiita_10422_metadata_encode.tsv", "--feature-metadata",
+        eg_dir + "taxonomy.tsv", "--output-dir",
+        "rankratioviz/tests/output/standalone_rrv_plot_basic", "--category",
+        "exposure_type_encode"
+    ])
+    assert result.exit_code == 0
+    # TODO now we can validate the rank plot and sample plot JSON and make sure
+    # it's good. Get the sample plot validation code from the old github page
+    # -- that'll satisfy one of the merging things.
