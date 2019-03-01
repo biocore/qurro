79c98d9f3b85162891c17aa49cb92fa91bad7385
Marcus Fedarko <mfedarko@ucsd.edu>
2019-02-28 20:13:27 -0800

    Re-enable Byrd 2017 test: close #53
    glad that this finally works with rrv again. took long enough :)
    
    See the README re the differentials file.

---
+++ rankratioviz/tests/input/byrd/README.txt
@@ -0,0 +1,4 @@
+The byrd_differentials.tsv file is just an old beta.csv file that I converted
+to the new format by changing commas to tabs and changing the filename: I can't
+make any guarantees as to its usefulness or accuracy beyond serving as
+a test songbird file.
--- rankratioviz/tests/test_byrd_basic.py
+++ rankratioviz/tests/test_byrd_basic.py
@@ -1,31 +1,31 @@
-# import os
-# from click.testing import CliRunner
-# # from rankratioviz import generate
-# from rankratioviz.tests import testing_utilities
-# import rankratioviz.scripts._plot as rrvp
-#
-#
-# def test_byrd():
-#     """Tests the JSON generation on the Byrd et al. 2017 dataset."""
-#
-#     byrd_input_dir = os.path.join("rankratioviz", "tests", "input", "byrd")
-#     out_dir = os.path.join("rankratioviz", "tests", "output", "byrd")
-#
-#     rank_json_loc = os.path.join(out_dir, "rank_plot.json")
-#     # sample_json_loc = os.path.join(out_dir, "sample_logratio_plot.json")
-#
-#     rloc = os.path.join(byrd_input_dir, "beta.csv")
-#     tloc = os.path.join(byrd_input_dir, "byrd_skin_table.biom")
-#     sloc = os.path.join(byrd_input_dir, "byrd_metadata.txt")
-#
-#     # Actually run the plot generation script
-#     runner = CliRunner()
-#     result = runner.invoke(rrvp.plot, [
-#         "--ranks", rloc, "--table", tloc, "--sample-metadata", sloc,
-#         "--output-dir", out_dir
-#     ])
-#     # Check that, at least, the test didn't cause any blatant errors
-#     assert result.exit_code == 0
-#     # Validate rank plot JSON
-#     testing_utilities.validate_rank_plot_json(rank_json_loc)
-#     # TODO call testing_utilities.validate_sample_plot_json() here
+import os
+from click.testing import CliRunner
+# from rankratioviz import generate
+from rankratioviz.tests import testing_utilities
+import rankratioviz.scripts._plot as rrvp
+
+
+def test_byrd():
+    """Tests the JSON generation on the Byrd et al. 2017 dataset."""
+
+    byrd_input_dir = os.path.join("rankratioviz", "tests", "input", "byrd")
+    out_dir = os.path.join("rankratioviz", "tests", "output", "byrd")
+
+    rank_json_loc = os.path.join(out_dir, "rank_plot.json")
+    # sample_json_loc = os.path.join(out_dir, "sample_logratio_plot.json")
+
+    rloc = os.path.join(byrd_input_dir, "byrd_differentials.tsv")
+    tloc = os.path.join(byrd_input_dir, "byrd_skin_table.biom")
+    sloc = os.path.join(byrd_input_dir, "byrd_metadata.txt")
+
+    # Actually run the plot generation script
+    runner = CliRunner()
+    result = runner.invoke(rrvp.plot, [
+        "--ranks", rloc, "--table", tloc, "--sample-metadata", sloc,
+        "--output-dir", out_dir
+    ])
+    # Check that, at least, the test didn't cause any blatant errors
+    assert result.exit_code == 0
+    # Validate rank plot JSON
+    testing_utilities.validate_rank_plot_json(rloc, rank_json_loc)
+    # TODO call testing_utilities.validate_sample_plot_json() here
