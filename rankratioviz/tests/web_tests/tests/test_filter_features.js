0245e7dffbdc205c7248688ea19aa6b7782ccef0
Marcus Fedarko <mfedarko@ucsd.edu>
2019-04-22 14:30:45 -0700

    TST: Add more feature filtering tests #2
    Eventually should make searching case-insensitive (see #30). In that
    case, will need to flip the "case sensitive" test I added here :)

--- rankratioviz/tests/web_tests/tests/test_filter_features.js
+++ rankratioviz/tests/web_tests/tests/test_filter_features.js
@@ -4,6 +4,7 @@ define(["feature_computation", "mocha", "chai"], function(
     chai
 ) {
     describe("Filtering lists of features based on text searching", function() {
+        // TODO add a test that uses the | as a rank delimiter?
         var inputFeatures = [
             "Feature 1",
             "Featurelol 2",
@@ -11,6 +12,17 @@ define(["feature_computation", "mocha", "chai"], function(
             "Feature 4|lol"
         ];
         var lolMatches = ["Featurelol 2", "Feature 4|lol"];
+
+        var inputTaxa = [
+            "Archaea;Crenarchaeota;Thermoprotei;Desulfurococcales;Desulfurococcaceae;Desulfurococcus;Desulfurococcus_kamchatkensis",
+            "Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus;Staphylococcus_aureus",
+            "Bacteria;Firmicutes;Bacilli;Bacillales;Staphylococcaceae;Staphylococcus;Staphylococcus_epidermidis",
+            "Viruses;Caudovirales;Myoviridae;Twortlikevirus;Staphylococcus_phage_Twort",
+            "Viruses;Caudovirales;Xanthomonas_phage_Xp15"
+        ];
+        var bacteriaMatches = [inputTaxa[1], inputTaxa[2]];
+        var caudoviralesMatches = [inputTaxa[3], inputTaxa[4]];
+        var staphTextMatches = [inputTaxa[1], inputTaxa[2], inputTaxa[3]];
 
         it("Finds matching features in text searching", function() {
             chai.assert.sameMembers(
@@ -28,6 +40,76 @@ define(["feature_computation", "mocha", "chai"], function(
                     "text"
                 ),
                 inputFeatures
+            );
+            // Text search should ignore taxonomic ranks (i.e. semicolons)
+            chai.assert.sameMembers(
+                feature_computation.filterFeatures(
+                    inputTaxa,
+                    "Staphylococcus",
+                    "text"
+                ),
+                staphTextMatches
+            )
+        });
+
+        it("Finds matching features in rank searching", function() {
+            chai.assert.sameMembers(
+                feature_computation.filterFeatures(
+                    inputTaxa,
+                    "Bacteria",
+                    "rank"
+                ),
+                bacteriaMatches
+            );
+            chai.assert.sameMembers(
+                feature_computation.filterFeatures(
+                    inputTaxa,
+                    "Caudovirales",
+                    "rank"
+                ),
+                caudoviralesMatches
+            );
+            // If all features don't have any taxonomic ranks, nothing should
+            // be found
+            chai.assert.isEmpty(
+                feature_computation.filterFeatures(
+                    inputFeatures,
+                    "Feature",
+                    "rank"
+                )
+            );
+            // Rank search should respect taxonomic ranks
+            chai.assert.sameMembers(
+                feature_computation.filterFeatures(
+                    inputTaxa,
+                    "Staphylococcus",
+                    "rank"
+                ),
+                bacteriaMatches
+            );
+        });
+
+        it("Searching is case sensitive", function() {
+            chai.assert.isEmpty(
+                feature_computation.filterFeatures(
+                    inputTaxa,
+                    "staphylococcus",
+                    "rank"
+                )
+            );
+            chai.assert.isEmpty(
+                feature_computation.filterFeatures(
+                    inputTaxa,
+                    "staphylococcus",
+                    "text"
+                )
+            );
+            chai.assert.isEmpty(
+                feature_computation.filterFeatures(
+                    inputFeatures,
+                    "feature",
+                    "text"
+                )
             );
         });
 
