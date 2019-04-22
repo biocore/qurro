a58d71c3386bc6d49c455cfb0e5bcf8a6391ee04
Marcus Fedarko <mfedarko@ucsd.edu>
2019-04-22 12:45:30 -0700

    STY: Fix styling of feature filtering test
    forgot to commit style fix in prev build -- will cancel that,
    since it'll make prettier fail

--- rankratioviz/tests/web_tests/tests/test_filter_features.js
+++ rankratioviz/tests/web_tests/tests/test_filter_features.js
@@ -6,7 +6,11 @@ define(["feature_computation", "mocha", "chai"], function(
     describe("Filtering lists of features based on text searching", function() {
         it('Throws an error if searchType isn\'t "rank" or "text"', function() {
             chai.assert.throws(function() {
-                feature_computation.filterFeatures([], "", "invalid searchtype!!!");
+                feature_computation.filterFeatures(
+                    [],
+                    "",
+                    "invalid searchtype!!!"
+                );
             });
         });
     });
