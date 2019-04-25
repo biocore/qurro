6bb6384c821042c49cfa17a57267f4034cb4819a
Marcus Fedarko <mfedarko@ucsd.edu>
2019-04-24 20:28:47 -0700

    TST: Make q2 intg tst order match standalone order
    IDK why it was different in the first place lol

--- rankratioviz/tests/test_integration_q2.py
+++ rankratioviz/tests/test_integration_q2.py
@@ -1,6 +1,14 @@
 from rankratioviz.tests.testing_utilities import run_integration_test
 
 
+def test_integration_q2_byrd():
+    """Tests rankratioviz on songbird output in the context of QIIME 2."""
+
+    run_integration_test("byrd", "q2_byrd", "byrd_differentials.tsv",
+                         "byrd_skin_table.biom", "byrd_metadata.txt",
+                         use_q2=True, q2_ranking_tool="songbird")
+
+
 def test_integration_q2_sleep_apnea():
     """Tests rankratioviz on DEICODE output in the context of QIIME 2."""
 
@@ -8,14 +16,6 @@ def test_integration_q2_sleep_apnea():
                          "qiita_10422_table.biom", "qiita_10422_metadata.tsv",
                          feature_metadata_name="taxonomy.tsv", use_q2=True,
                          q2_ranking_tool="DEICODE")
-
-
-def test_integration_q2_byrd():
-    """Tests rankratioviz on songbird output in the context of QIIME 2."""
-
-    run_integration_test("byrd", "q2_byrd", "byrd_differentials.tsv",
-                         "byrd_skin_table.biom", "byrd_metadata.txt",
-                         use_q2=True, q2_ranking_tool="songbird")
 
 
 def test_integration_q2_red_sea():
