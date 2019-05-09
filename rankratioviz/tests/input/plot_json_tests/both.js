ef824acce0d09952352e679e68de6c0778968b8b
Marcus Fedarko <mfedarko@umd.edu>
2019-05-08 23:37:18 -0700

    TST: Add some basic example data for _plot_utils
    This will be useful when writing tests for this in the future. #2

---
+++ rankratioviz/tests/input/plot_json_tests/both.js
@@ -0,0 +1,2 @@
+var rankPlotJSON = {};
+var samplePlotJSON = {};
---
+++ rankratioviz/tests/input/plot_json_tests/both_full.js
@@ -0,0 +1,2 @@
+var rankPlotJSON = {"a": "b", "data": {"name": "asdf"}, "datasets": {"asdf": {1: 2}}}
+var samplePlotJSON = {"c": "d", "data": {"name": "what"}, "datasets": {"what": {3: 4}}}
