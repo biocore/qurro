2e792de8e5c9a45831dee492ddf19f8625b50a36
Marcus Fedarko <mfedarko@umd.edu>
2019-05-08 20:36:23 -0700

    REL: Update classifers in setup.py
    Ideally we should really hash out which version(s) of python are
    required, but for now just restricting to 3.5/3.6 should work
    
    if poss it'd be nice to support python < 3.5, but no biggie if not

--- setup.py
+++ setup.py
@@ -17,11 +17,11 @@ from setuptools import find_packages, setup
 classes = """
     Development Status :: 3 - Alpha
     License :: OSI Approved :: BSD License
-    Topic :: Software Development :: Libraries
     Topic :: Scientific/Engineering
     Topic :: Scientific/Engineering :: Bio-Informatics
     Topic :: Scientific/Engineering :: Visualization
-    Programming Language :: Python :: 3
+    Programming Language :: Python :: 3.5
+    Programming Language :: Python :: 3.6
     Programming Language :: Python :: 3 :: Only
     Operating System :: Unix
     Operating System :: POSIX
