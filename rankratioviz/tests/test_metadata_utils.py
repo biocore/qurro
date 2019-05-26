08927ba3f1623b559df2c6c9774097324413138c
Marcus Fedarko <mfedarko@umd.edu>
2019-05-26 02:07:11 -0700

    TST: Use files instead of StringIO in MD util test
    StringIO was causing some problems with the new code that detects
    q2 comment lines, so out the door it goes

--- rankratioviz/tests/test_metadata_utils.py
+++ rankratioviz/tests/test_metadata_utils.py
@@ -1,4 +1,4 @@
-from io import StringIO
+import os
 from rankratioviz._metadata_utils import read_metadata_file
 
 
@@ -10,8 +10,9 @@ def test_read_metadata_file():
        won't get turned into true / false in the JS side of things), and
        reduces headaches for us all around.
     """
-    header = "ID\tMD1\tMD2\n"
-    simple_md = StringIO(header + "S1\t1.0\t2.0\n" + "S2\t3.0\t4.0")
+    simple_md = os.path.join(
+        "rankratioviz", "tests", "input", "metadata_tests", "md1.tsv"
+    )
     simple_df = read_metadata_file(simple_md)
     assert simple_df.dtypes[0] == "float64"
     assert simple_df.dtypes[1] == "float64"
@@ -20,7 +21,9 @@ def test_read_metadata_file():
     assert simple_df.at["S1", "MD2"] == 2.0
     assert simple_df.at["S2", "MD2"] == 4.0
 
-    has_bool_md = StringIO(header + "S1\tTrue\t5\n" + "S2\tFalse\t6")
+    has_bool_md = os.path.join(
+        "rankratioviz", "tests", "input", "metadata_tests", "md2.tsv"
+    )
     has_bool_df = read_metadata_file(has_bool_md)
     # This should be an object, NOT a bool! This is because
     # read_metadata_file() explicitly converts bool columns to object columns
@@ -32,8 +35,8 @@ def test_read_metadata_file():
     assert has_bool_df.at["S1", "MD2"] == 5
     assert has_bool_df.at["S2", "MD2"] == 6
 
-    one_bool_md = StringIO(
-        header + "S1\tTrue\t5\n" + "S2\tWHAAAT\tMissing: Not provided"
+    one_bool_md = os.path.join(
+        "rankratioviz", "tests", "input", "metadata_tests", "md3.tsv"
     )
     one_bool_df = read_metadata_file(one_bool_md)
     assert one_bool_df.dtypes[0] == "object"
