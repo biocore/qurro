478c5a484b686e3423889dbac860a20fb448b8b3
Marcus Fedarko <mfedarko@ucsd.edu>
2019-07-08 14:32:50 -0700

    REL: bump version to 0.2.0
    :D

--- qurro/__init__.py
+++ qurro/__init__.py
@@ -10,5 +10,5 @@
 # The full license is in the file LICENSE.txt, distributed with this software.
 # ----------------------------------------------------------------------------
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 name = "qurro"
