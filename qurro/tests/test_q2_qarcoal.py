b807628d87d21cb9035d2f7e12f04c1a89228dfd
Marcus Fedarko <mfedarko@ucsd.edu>
2022-10-19 22:19:51 -0700

    STY: Re-format python using black 21.12b0
    Since we pinned it in this PR. Shoulda done this with the previous
    commit, whoops

--- qurro/tests/test_q2_qarcoal.py
+++ qurro/tests/test_q2_qarcoal.py
@@ -432,9 +432,9 @@ class TestIrregularData:
         """
         samps = ["S{}".format(i) for i in range(3)]
         feats = ["F{}".format(i) for i in range(6)]
-        s0 = [x**53 for x in (2.0, 3.2, 4.1, 6.2, 4.3, 3.7)]
-        s1 = [x**53 for x in (2.5, 2.6, 2.9, 5.2, 2.1, 2.5)]
-        s2 = [x**53 for x in (2.2, 3.5, 3.1, 3.0, 2.2, 4.0)]
+        s0 = [x ** 53 for x in (2.0, 3.2, 4.1, 6.2, 4.3, 3.7)]
+        s1 = [x ** 53 for x in (2.5, 2.6, 2.9, 5.2, 2.1, 2.5)]
+        s2 = [x ** 53 for x in (2.2, 3.5, 3.1, 3.0, 2.2, 4.0)]
         confidence = ["0.99"] * 6
         tax_labels = [
             "Charmander",
